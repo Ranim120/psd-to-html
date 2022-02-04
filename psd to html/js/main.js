7ec919cb4b71c27dbca7e0c986044a1916afba4d
Md Abdur Rahim <94445824+Ranim120@users.noreply.github.com>
2022-02-04 23:16:08 +0600

    Add files via upload

---
+++ psd to html/js/main.js
@@ -0,0 +1,9 @@
+//Bx Slider--------------->
+
+$(document).ready(function(){
+    $('.slider-area').bxSlider();
+    jQuery('#main-menu').meanmenu({
+      meanMenuContainer: '.mobile-menu',
+      meanScreenWidth: 991,
+    });
+  });
